066793cfda189c8ac361c0b930e09c1d2b0636e1
yyzq <yyzq@qq.com>
2021-08-15 19:49:36 +0800

    我也增加一个版本库吧

--- src/main/java/com/yyzq/springbootmybatisplus/SpringbootMybatisplusApplication.java
+++ src/main/java/com/yyzq/springbootmybatisplus/SpringbootMybatisplusApplication.java
@@ -11,7 +11,9 @@ public class SpringbootMybatisplusApplication {
     }
 
     public void test123(){
-        System.out.println("中国我最大");
+        System.out.println("中国我最大啊");
+        System.out.println("宇宙我最大");
     }
+
 
 }
